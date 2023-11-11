7622a2dba4872384e4072d617bf3ee969f3ab4de
Benwbs <150517006+Benwbs@users.noreply.github.com>
2023-11-11 15:42:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Form from './components/Form';
 
 function App() {
-  return {
+  return (
     <>
-    <Form></Form>
+      <Header></Header>
+      <Form></Form>
     </>
   );
 }
